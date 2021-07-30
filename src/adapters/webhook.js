2e3187f3eb92707a0d838167f23c03e4eadbe3dc
Chris Troutner <chris.troutner@gmail.com>
2021-07-30 10:20:19 -0700

    more linting

--- src/adapters/webhook.js
+++ src/adapters/webhook.js
@@ -2,40 +2,40 @@
   The (Clean Architecture) Adapter for manageing a webhook connection to P2WDB.
 */
 
-const config = require("../../config");
-const axios = require("axios");
+const config = require('../../config')
+const axios = require('axios')
 
-let _this;
+let _this
 
 class WebHook {
-  constructor() {
-    _this = this;
-    _this.config = config;
-    _this.axios = axios;
+  constructor () {
+    _this = this
+    _this.config = config
+    _this.axios = axios
   }
 
   // REST petition to create a webhook in p2wdb-service
-  async createWebHook(url) {
+  async createWebHook (url) {
     try {
-      if (!url || typeof url !== "string") {
-        throw new Error("url must be a string");
+      if (!url || typeof url !== 'string') {
+        throw new Error('url must be a string')
       }
 
-      const endpoint = _this.config.webhookService;
+      const endpoint = _this.config.webhookService
 
       const obj = {
-        appId: "torlist",
+        appId: 'torlist',
         url
-      };
+      }
 
-      const result = await axios.post(endpoint, obj);
+      const result = await axios.post(endpoint, obj)
 
-      return result.data;
+      return result.data
     } catch (err) {
-      console.log("Error in adapters/webhook/createWebHook()");
-      throw err;
+      console.log('Error in adapters/webhook/createWebHook()')
+      throw err
     }
   }
 }
 
-module.exports = WebHook;
+module.exports = WebHook
