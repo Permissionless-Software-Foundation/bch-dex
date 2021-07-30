60a2c49863c30ca18167027c4d81afef3073340b
Chris Troutner <chris.troutner@gmail.com>
2021-07-30 10:20:05 -0700

    linting

--- test/unit/adapters/webhook.adapter.unit.js
+++ test/unit/adapters/webhook.adapter.unit.js
@@ -2,67 +2,67 @@
   Unit tests for the webhook adapter.
 */
 
-const assert = require('chai').assert
-const sinon = require('sinon')
+const assert = require("chai").assert;
+const sinon = require("sinon");
 
 // Set the environment variable to signal this is a test.
-process.env.SVC_ENV = 'test'
+process.env.SVC_ENV = "test";
 
-const WebHook = require('../../../src/adapters/webhook')
+const WebHook = require("../../../src/adapters/webhook");
 
-describe('#Webhook-Adapter', () => {
-  let uut
-  let sandbox
+describe("#Webhook-Adapter", () => {
+  let uut;
+  let sandbox;
 
   beforeEach(async () => {
-    uut = new WebHook()
-    sandbox = sinon.createSandbox()
-  })
+    uut = new WebHook();
+    sandbox = sinon.createSandbox();
+  });
 
-  afterEach(() => sandbox.restore())
+  afterEach(() => sandbox.restore());
 
-  describe('#createWebHook', () => {
-    it('should throw error if input is not provided', async () => {
+  describe("#createWebHook", () => {
+    it("should throw error if input is not provided", async () => {
       try {
-        await uut.createWebHook()
-        assert.fail('unexpected code path')
+        await uut.createWebHook();
+        assert.fail("unexpected code path");
       } catch (err) {
-        assert.include(err.message, 'url must be a string')
+        assert.include(err.message, "url must be a string");
       }
-    })
-    it('should throw error if input is not string', async () => {
-      try {
-        await uut.createWebHook(1)
-        assert.fail('unexpected code path')
-      } catch (err) {
-        assert.include(err.message, 'url must be a string')
-      }
-    })
-    it('should handle error', async () => {
-      try {
-        sandbox
-          .stub(uut.axios, 'post')
-          .throws(new Error('test error'))
-        await uut.createWebHook('https://test.com/my-webhook')
-        assert.fail('unexpected code path')
-      } catch (err) {
-        assert.include(err.message, 'test error')
-      }
-    })
+    });
 
-    it('should return response', async () => {
+    it("should throw error if input is not string", async () => {
+      try {
+        await uut.createWebHook(1);
+        assert.fail("unexpected code path");
+      } catch (err) {
+        assert.include(err.message, "url must be a string");
+      }
+    });
+
+    it("should handle error", async () => {
+      try {
+        sandbox.stub(uut.axios, "post").throws(new Error("test error"));
+        await uut.createWebHook("https://test.com/my-webhook");
+        assert.fail("unexpected code path");
+      } catch (err) {
+        assert.include(err.message, "test error");
+      }
+    });
+
+    it("should return response", async () => {
       sandbox
-        .stub(uut.axios, 'post')
-        .resolves({ data: { success: true, id: '61018c8c9a71973a596cdccb' } })
+        .stub(uut.axios, "post")
+        .resolves({ data: { success: true, id: "61018c8c9a71973a596cdccb" } });
 
-      const url = 'https://test.com/my-webhook'
-      const result = await uut.createWebHook(url)
+      const url = "https://test.com/my-webhook";
+      const result = await uut.createWebHook(url);
 
-      assert.isObject(result)
-      assert.property(result, 'success')
-      assert.property(result, 'id')
-      assert.isBoolean(result.success)
-      assert.isString(result.id)
-    })
-  })
-})
+      assert.isObject(result);
+      assert.property(result, "success");
+      assert.property(result, "id");
+      assert.isBoolean(result.success);
+      assert.isString(result.id);
+    });
+  });
+});
