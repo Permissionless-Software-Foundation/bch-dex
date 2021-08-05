49aac384fc20fa5bc222eb9aab80d5246a47bfc4
Chris Troutner <chris.troutner@gmail.com>
2021-08-05 06:51:05 -0700

    fix(mongo): Changing the name of the MongoDB database

--- config/env/development.js
+++ config/env/development.js
@@ -7,6 +7,6 @@
 module.exports = {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/ipfs-service-dev',
+  database: 'mongodb://localhost:27017/torlist-service-dev',
   env: 'dev'
 }
