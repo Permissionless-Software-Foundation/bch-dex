4557761d088c03dcc9b1a4e7a4120d330c311331
Chris Troutner <chris.troutner@gmail.com>
2021-09-17 10:00:21 -0700

    Added pseudo code for next steps

--- src/use-cases/offer.js
+++ src/use-cases/offer.js
@@ -23,14 +23,22 @@ class OfferLib {
     try {
       // console.log('createOffer(entryObj): ', entryObj)
 
+      // Input Validation
+      const offerEntity = this.offerEntity.validate(entryObj)
+      // console.log('offerEntity: ', offerEntity)
+
+      // Ensure sufficient tokens exist to create the offer.
+      // await this.ensureFunds(offerEntity)
+
+      // Move the tokens to holding address.
+      // const utxoInfo = await moveTokens(offerEntity)
+
+      // Update the offer with the new UTXO information.
+
       // Burn PSF token to pay for P2WDB write.
       const txid = await this.adapters.wallet.burnPsf()
       console.log('burn txid: ', txid)
       console.log(`https://simpleledger.info/tx/${txid}`)
-
-      // Input Validation
-      const offerEntity = this.offerEntity.validate(entryObj)
-      // console.log('offerEntity: ', offerEntity)
 
       // TODO: Move tokens to holding address, which will generate the UTXO to
       // use in the Offer.
@@ -58,6 +66,17 @@ class OfferLib {
       // console.log("Error in use-cases/entry.js/createEntry()", err.message)
       wlogger.error('Error in use-cases/entry.js/createOffer())')
       throw err
+    }
+  }
+
+  // Move the tokens indicated in the offer to a temporary holding address.
+  // This will generate the UTXO used in the Signal message. This function
+  // moves the funds and returns the UTXO information.
+  async moveTokens () {
+    try {
+      //
+    } catch (err) {
+      console.error('Error in moveTokens()')
     }
   }
 }
