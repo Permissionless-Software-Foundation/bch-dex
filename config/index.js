ccd6bcb6fd1a7af0548807c703311f73c3a5fe97
Chris Troutner <chris.troutner@gmail.com>
2021-11-24 15:21:09 -0800

    fix(CI/CD): Testing CI/CD system

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
-const common = require("./env/common");
+const common = require('./env/common')
 
-const env = process.env.AVAX_DEX_ENV || "development";
-const config = require(`./env/${env}`);
+const env = process.env.SLP_ENV || 'development'
+const config = require(`./env/${env}`)
 
-module.exports = Object.assign({}, common, config);
+module.exports = Object.assign({}, common, config)
