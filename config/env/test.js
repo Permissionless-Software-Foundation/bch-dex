0596e1749754418afaf1237ba51bcf84d8285ba2
Chris Troutner <chris.troutner@gmail.com>
2021-11-24 11:41:29 -0800

    fix(deps): Updated dependencies

--- config/env/test.js
+++ config/env/test.js
@@ -7,6 +7,6 @@
 module.exports = {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/torlist-service-test',
+  database: 'mongodb://localhost:27017/swap-service-test',
   env: 'test'
 }
