0b940c797c366600e3d51e282712138f38198fab
Chris Troutner <chris.troutner@gmail.com>
2022-03-26 12:05:11 -0700

    feat(rpi): Created customized docker files for RPi4

---
+++ production/rpi-docker/bch-dex/start-production.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# BEGIN: Optional configuration settings
+
+# The human readable name this IPFS node identifies as.
+export COORD_NAME=bch-dex-generic
+
+# Allow this node to function as a circuit relay. It must not be behind a firewall.
+#export ENABLE_CIRCUIT_RELAY=true
+# For browsers to use your circuit realy, you must set up a domain, SSL certificate,
+# and you must forward that subdomain to the IPFS_WS_PORT.
+#export CR_DOMAIN=subdomain.yourdomain.com
+
+# Debug level. 0 = minimal info. 2 = max info.
+export DEBUG_LEVEL=1
+
+# END: Optional configuration settings
+
+
+# Production database connection string.
+export DBURL=mongodb://172.17.0.1:5666/bch-swap-service-prod
+
+# Configure REST API port
+export PORT=5700
+
+# bch-dex specific env vars
+export BCH_ENV=production
+export WEBHOOKSERVICE=http://172.17.0.1:5667/webhook
+export WEBHOOKTARGET=http://172.17.0.1:5700/order
+export APP_ID=bch-dex-test557
+
+npm start
