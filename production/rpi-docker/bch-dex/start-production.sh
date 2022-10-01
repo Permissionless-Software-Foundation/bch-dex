6d285c77cd33fdeb2a68645818db1dbf5e613fd3
Chris Troutner <chris.troutner@gmail.com>
2022-10-01 15:43:49 -0700

    fix(RPi): Updating docker images

--- production/rpi-docker/bch-dex/start-production.sh
+++ production/rpi-docker/bch-dex/start-production.sh
@@ -24,7 +24,7 @@ export DBURL=mongodb://172.17.0.1:5666/bch-swap-service-prod
 export PORT=5700
 
 # bch-dex specific env vars
-export BCH_DEX=production
+export BCH_DEX=prod
 export WEBHOOKSERVICE=http://172.17.0.1:5667/webhook
 export WEBHOOKTARGET=http://172.17.0.1:5700/p2wdb
 export APP_ID=bch-dex-001
